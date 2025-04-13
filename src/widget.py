6f08a6f04d5e9ded7c076ec7aed62191429354f9
lysikov_valera777@hotmail.com <lysikov_valera777@hotmail.com>
2025-04-13 11:43:39 +0700

    Added get_date function

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def mask_account_card(account_info: str) -> str:
     """
     Маскирует номер карты или счета.
@@ -16,3 +19,14 @@ def mask_account_card(account_info: str) -> str:
         number = parts[-1]
         masked_number = f"{number[:4]} {number[4:6]}** **** {number[-4:]}"
         return " ".join(parts[:-1]) + " " + masked_number
+
+
+def get_date(date_str: str) -> str:
+    """
+    Преобразует дату из формата ISO в формат ДД.ММ.ГГГГ.
+
+    :param date_str: Строка с датой в формате ISO (например, "2024-03-11T02:26:18.671407").
+    :return: Строка с датой в формате ДД.ММ.ГГГГ.
+    """
+    date_obj = datetime.fromisoformat(date_str)
+    return date_obj.strftime("%d.%m.%Y")
